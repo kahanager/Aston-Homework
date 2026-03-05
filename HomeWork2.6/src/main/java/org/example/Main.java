296676d41b76edaaf641b7657d07ca6a46491556
kahanager <kahanager@gmail.com>
2026-03-05 15:59:16 +0300

    проект

---
+++ HomeWork2.6/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> studentList = new ArrayList<>();
+        studentList.add(new Student("Иванов","Группа А", 1, 4.2));
+
+
+    }
+}
---
+++ HomeWork2.6/src/main/java/org/example/Student.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Student {
+    private String name;
+    private String group;
+    private int course;
+    private double avaragePoint;
+    public Student(String name, String group, int course, double avaragePoint) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.avaragePoint = avaragePoint;
+    }
+}
